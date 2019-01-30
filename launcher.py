f94a845d13f886414de64297812d0f83529924e6
ByskyXie <547404396@qq.com>
2019-01-30 22:56:33 +0800

    Keywords

--- launcher.py
+++ launcher.py
@@ -240,7 +240,16 @@ class Launcher:
             output_file.writelines('[Max Allow Duration]:' + str(self.__round_max_duration))
 
     def get_commodity_type_list(self) -> list:
-        list = ['U盘', '手机']
+        list = []
+        try:
+            file = open("keywords.txt")
+            while True:
+                kw = file.readline()
+                if not kw:
+                    break
+                list.append(kw)
+        except FileNotFoundError:
+            return ['U盘', '手机']
         # 从外部文件读取搜索列表
         return list
 

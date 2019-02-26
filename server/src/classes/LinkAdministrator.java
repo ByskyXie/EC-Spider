e06d31619a8eb3e6e26780111905d8a911e99ac5
ByskyXie <547404396@qq.com>
2019-02-26 23:45:09 +0800

    User interface by G2.

--- server/src/classes/LinkAdministrator.java
+++ server/src/classes/LinkAdministrator.java
@@ -18,17 +18,17 @@ import java.util.Properties;
 
 public class LinkAdministrator {
 	private static final String host = "smtp.qq.com";
-	private static final int port = 465;
+	private static final String port = "587";
 	private static final String reciver = "byskyxie@qq.com";
 	private static final String to = "byskyXie";
 
 	private MimeMessage createMessage(Session session, String from, String to, String title, String msg) throws MessagingException, UnsupportedEncodingException {
 		MimeMessage mm = new MimeMessage(session);
-		mm.setFrom(new InternetAddress(from, "EC-Spider", "UTF-8"));
+		mm.setFrom(new InternetAddress(from, "EC-Spider Server", "UTF-8"));
 		mm.setRecipient(MimeMessage.RecipientType.TO,
 				new InternetAddress(to, "ByskyXie", "UTF-8"));
 		mm.setSubject(title, "UTF-8");
-		mm.setContent(msg, "UTF-8");
+		mm.setText(msg, "UTF-8");
 		mm.setSentDate(new Date());
 		mm.saveChanges();
 		return mm;
@@ -37,33 +37,21 @@ public class LinkAdministrator {
 	public void sendMessage(String title, String msg, String usr, String pwd) {
 		Properties props = new Properties();
 		props.setProperty("mail.transport.protocol", "smtp");
-		props.setProperty("mail.smtp.host", host);
-		props.setProperty("mail.smtp.auth", "false");
+		props.setProperty("mail.smtp.host", host);  //smtp服务器
+		props.setProperty("mail.smtp.port", port);
+		props.setProperty("mail.smtp.auth", "true");  //身份验证
+		props.setProperty("mail.user", usr);
+		props.setProperty("mail.password", pwd);
 		Session session = Session.getDefaultInstance(props);
 		MimeMessage mime = null;
 		try {
-			mime = createMessage(session, usr, pwd, title, msg);
+			mime = createMessage(session, usr, reciver, title, msg);
+			Transport tran = session.getTransport();
+			tran.connect(usr, pwd);
+			tran.sendMessage(mime, mime.getAllRecipients());
+			tran.close();
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
-		Transport tran = null;
-		try {
-			tran = session.getTransport();
-			tran.connect(usr, pwd);
-			tran.send(mime, mime.getAllRecipients());
-		} catch (NoSuchProviderException e) {
-			e.printStackTrace();
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
-		
-		
-		
-		try {
-			if(tran != null)
-				tran.close();
 		} catch (MessagingException e) {
 			e.printStackTrace();
 		}
--- server/src/servlet/PopupServlet.java
+++ server/src/servlet/PopupServlet.java
@@ -1,6 +1,10 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,17 +21,32 @@ import classes.DBHelper;
 public class PopupServlet extends HttpServlet {
 	private final DBHelper helper = new DBHelper();
 	private static final long serialVersionUID = 1L;
+	private static final int point = 365/2;  //默认显示历史价格区间
+	private static final String SQL_QUERY = 
+			"SELECT data_begin_time,item_price "
+			+ "FROM item "
+			+ "WHERE item_url_md5='%s' "
+			+ "ORDER BY data_begin_time ASC;";
 
     public PopupServlet() {
-
+    	super();
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter pw = response.getWriter();
+		String urlMD5 = request.getParameter("md5");
+		try {
+			ResultSet rs = helper.execute(String.format(SQL_QUERY, urlMD5));
+		} catch (SQLException e) {
+			e.printStackTrace();
+			pw.append("");  //TODO:显示错误
+			return;
+		}
+		pw.append("Served at: ");
+		pw.close();
 	}
 
 	/**
